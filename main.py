8dacde954306f78577d2a35f6a7708923e372e55
Daniel Morala <morala84@gmail.com>
2022-03-18 19:54:44 +0100

    Variables checkedAHU and counterTags added

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ database = os.getenv('DATABASE')
 #date1 = input('Introduce la primera fecha (yyyy-mm-dd):')
 #date2 = input('Introduce la segunda fecha (yyyy-mm-dd):')
 
-date1 = '2022-02-15'
-date2 = '2022-03-17'
+date1 = '2022-03-13'
+date2 = '2022-03-20'
+checkedAHU = 0
 
 jsonResponse = inspections_list.todoList(tokenNotion, database, date1, date2)
 jsonData = json.loads(jsonResponse)
@@ -29,9 +30,6 @@ for data in jsonData['results']:
     jsonMo = jsonProperties['MO']
     print(jsonMo['number'])
     
-    jsonTags = jsonProperties['Tags']
-    jsonMultiSelect = jsonTags['multi_select']
-
     jsonOrder = jsonProperties['Pedido']
     jsonOrderTitle = jsonOrder['title']
     
@@ -44,12 +42,20 @@ for data in jsonData['results']:
     for dataModel in jsonModel_rich_text:
         print(dataModel['plain_text'])
 
+    jsonTags = jsonProperties['Tags']
+    jsonMultiSelect = jsonTags['multi_select']
+    
+    counterTags = 0
     for dataName in jsonMultiSelect:
-        print(dataName['name'])
+        nameTag = dataName['name']
+        counterTags = counterTags + 1
+        print(f'[{counterTags}] {nameTag}')
         
     jsonInspector = jsonProperties['Inspector']
     jsonInspectorSelect = jsonInspector['select']
     jsonInspectorName = jsonInspectorSelect['name']
+    checkedAHU = checkedAHU + 1
     print('Inspector: ' + jsonInspectorName)
     print('Numero de lineas: ' + counter_lines.counterLines(tokenNotion, jsonId))
     print('************************************************')
+print(f'Unidades revisadas: {checkedAHU}')
