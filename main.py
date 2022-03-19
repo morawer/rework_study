24921167ccf02c0bd597bb4e3d00d69f90075be4
Daniel Morala <morala84@gmail.com>
2022-03-19 19:25:17 +0100

    dataArray added

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ database = os.getenv('DATABASE')
 date1 = '2022-03-13'
 date2 = '2022-03-20'
 checkedAHU = 0
+dataArray= []
 
 jsonResponse = inspections_list.todoList(tokenNotion, database, date1, date2)
 jsonData = json.loads(jsonResponse)
@@ -24,23 +25,37 @@ for data in jsonData['results']:
     jsonDate = data["created_time"]
     Date2 = jsonDate.split("T")[0].split("-")
     DateFinal = "/".join(reversed(Date2))
+    dataArray.append(DateFinal)
     print(DateFinal)
     
     jsonProperties = data['properties']
-    jsonMo = jsonProperties['MO']
-    print(jsonMo['number'])
-    
     jsonOrder = jsonProperties['Pedido']
     jsonOrderTitle = jsonOrder['title']
-    
     for dataOrder in jsonOrderTitle:
         dataOrderText = dataOrder['text']
+        dataArray.append(dataOrderText['content'])
         print(dataOrderText['content'])
+    
+    jsonInspector = jsonProperties['Inspector']
+    jsonInspectorSelect = jsonInspector['select']
+    jsonInspectorName = jsonInspectorSelect['name']
+    dataArray.append(jsonInspectorName)
+    print('Inspector: ' + jsonInspectorName)
+ 
+    jsonProperties = data['properties']
+    jsonMo = jsonProperties['MO']
+    dataArray.append(jsonMo['number'])
+    print(jsonMo['number'])
     
     jsonModel = jsonProperties['Modelo']
     jsonModel_rich_text = jsonModel['rich_text']
     for dataModel in jsonModel_rich_text:
+        dataArray.append(dataModel['plain_text'])
         print(dataModel['plain_text'])
+        
+    countLines = counter_lines.counterLines(tokenNotion, jsonId)
+    print('Numero de lineas: ' + countLines)
+    dataArray.append(countLines)
 
     jsonTags = jsonProperties['Tags']
     jsonMultiSelect = jsonTags['multi_select']
@@ -50,12 +65,10 @@ for data in jsonData['results']:
         nameTag = dataName['name']
         counterTags = counterTags + 1
         print(f'[{counterTags}] {nameTag}')
+        dataArray.append(nameTag)
         
-    jsonInspector = jsonProperties['Inspector']
-    jsonInspectorSelect = jsonInspector['select']
-    jsonInspectorName = jsonInspectorSelect['name']
     checkedAHU = checkedAHU + 1
-    print('Inspector: ' + jsonInspectorName)
-    print('Numero de lineas: ' + counter_lines.counterLines(tokenNotion, jsonId))
+    print(dataArray)
+    dataArray.clear()
     print('************************************************')
 print(f'Unidades revisadas: {checkedAHU}')
