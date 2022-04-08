0183d7c2e01a71361a19af9412c55921b8ee0243
Daniel Morala <morala84@gmail.com>
2022-04-08 18:30:07 +0200

    with html problems in Outlook

--- sender_email.py
+++ sender_email.py
@@ -15,24 +15,22 @@ def sabanaList(sabanaArray, avgLines):
         <head>
         <meta charset="utf-8">
         <title>HTML</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="estilo.css">
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">        
         </head>
         <body>
             <header>
             <div style = "text-align:center;">
-            <img src="https://mussun.com/wp-content/uploads/2019/08/Systemair_logo-768x237.jpg" alt="www.systemair.es" width="390" height="170">
+            <img src="https://mussun.com/wp-content/uploads/2019/08/Systemair_logo-768x237.jpg" alt="www.systemair.es">
             </div>
             </header>
-        <section style= "padding: 30px; text-align: left; background: #1a4c7f; color: white; font-family: 'Lato', sans-serif;">
+            <div style= "background-color: #1a4c7f; text-align: left; color: white; font-family: 'Lato', sans-serif;">
         '''
 
     htmlBodyTitle = f'''
                 <h1>Se han revisado {sabanaLenght} UTA's</h1>'''
 
     htmlBodyTable = f'''
-                <table style= "border-collapse: collapse; width: 100%; padding: 10px; background-color: #1a4c7f; font-size: 14px;">
+                <table style= "width: 100%; background-color: #1a4c7f; font-size: 14px;">
                     <tr>
                         <th style= "text-align: center; padding: 8px; color: white; font-size: 18px;">Pedido</th>
                         <th style= "text-align: center; padding: 8px; color: white; font-size: 18px;">MO</th>
@@ -45,8 +43,8 @@ def sabanaList(sabanaArray, avgLines):
 
     html_body_end = f'''
                 </table>
-                <h3>Los equipos tienen una media de {avgLines:.1f} líneas.</h3>
-        </section>
+                <h3 style = "color: white">Los equipos tienen una media de {avgLines:.1f} líneas.</h3>
+            </div>
         </body>
         </html>
         '''
@@ -56,13 +54,13 @@ def sabanaList(sabanaArray, avgLines):
             html_sabana = html_sabana + '<tr style = "background-color: #33689D;"><td style = "text-align: center; padding: 8px; color: white;"><a href=' + sabana.url + ' style = "color: white; background-color: transparent; text-decoration: none; font-weight: bold;" target= "_blank">' + sabana.order + '</td><td style= "text-align: center; padding: 8px; color: white;">' + str(sabana.mo) + '</td><td style= "text-align: center; padding: 8px; color: white;">' + sabana.model + \
                 '</td><td style= "text-align: center; padding: 8px; color: white;">' + sabana.inspector + \
                 '</td><td style= "text-align: center; padding: 8px; color: white;">' + \
-                str(sabana.lines) + '</td>'
+                str(sabana.lines) + '</td></tr>'
         else:
-            html_sabana = html_sabana + '<tr style = "background-color: #1a4c7f;"><td style = "text-align: center; padding: 8px;color: white;"><a href=' + sabana.url + ' style = "color: white; background-color: transparent; text-decoration: none; font-weight: bold;" target= "_blank">' + sabana.order + '</td><td style= "text-align: center; padding: 8px; color: white;">' + str(sabana.mo) + \
+            html_sabana = html_sabana + '<tr style = "background-color: #1a4c7f;"><td style = "text-align: center; padding: 8px; color: white;"><a href=' + sabana.url + ' style = "color: white; background-color: transparent; text-decoration: none; font-weight: bold;" target= "_blank">' + sabana.order + '</td><td style= "text-align: center; padding: 8px; color: white;">' + str(sabana.mo) + \
                 '</td><td style= "text-align: center; padding: 8px; color: white;">' + sabana.model + \
                 '</td><td style= "text-align: center; padding: 8px; color: white;">' + sabana.inspector + \
                 '</td><td style= "text-align: center; padding: 8px; color: white;">' + \
-                str(sabana.lines) + '</td>'
+                str(sabana.lines) + '</td></tr>'
         a = a + 1
     htmlEmail = html_body + htmlBodyTitle + \
         htmlBodyTable + html_sabana + html_body_end
@@ -78,7 +76,7 @@ def sendEmail(mail_subject, mail_body, avgLines):
 
     mimemsg = MIMEMultipart()
     mimemsg['From'] = mail_from
-    mimemsg['To'] = mail_to
+    mimemsg['To'] = 'daniel.morala@systemair.es'
     mimemsg['Subject'] = mail_subject
     mimemsg.attach(MIMEText(sabanaList(mail_body, avgLines), 'html'))
     try:
