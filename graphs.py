21ca4e3f41dd7dada262995ed97ee9b89cf93a90
Daniel Morala <morala84@gmail.com>
2024-01-21 11:33:42 +0100

    cambiado a primer dia de la semana comenzando el lunes

--- graphs.py
+++ graphs.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 def graphsAvgCreator():
-    
+
     weekNumberAux = 0
     linesAcum = 0
     linesAvg = 0
@@ -40,7 +40,7 @@ def graphsAvgCreator():
                     linesAcum = 0
                     weeksAcum = 0
                     initWeek = True
-                    
+
                 elif row == lastRow:
                     weekNumberAux = weeknumber
                     linesRow = sheet.cell(row=row, column=6)
@@ -53,7 +53,7 @@ def graphsAvgCreator():
                     lines.append(float("{:.1f}".format(linesAvg)))
                     counterAHU.append(weeksAcum)
                     break
-                
+
                 weekNumberAux = weeknumber
                 linesAcum = 0
                 linesRow = sheet.cell(row=row, column=6)
@@ -73,7 +73,7 @@ def graphsAvgCreator():
                 counterAHU.remove(counterAHU[0])
     else:
         print('El archivo no existe')
-        
+
     def totalAvgLines():
         avgAcum = 0
         for avg in lines:
@@ -85,31 +85,32 @@ def graphsAvgCreator():
     x = np.arange(len(dates))
     width = 0.35
     bar1 = ax.bar(x - width/2, counterAHU, width, label="Nº UTA's")
-    bar2 = ax.bar(x + width/2, lines, width, label= "Media de lineas")
-    
+    bar2 = ax.bar(x + width/2, lines, width, label="Media de lineas")
+
     ax.set_xlabel('Semanas')
-    ax.set_title(f"Número de UTA's y media de líneas por semana | totalAvg: {totalAvgLines:.1f}")
+    ax.set_title(
+        f"Número de UTA's y media de líneas por semana | totalAvg: {totalAvgLines:.1f}")
     ax.set_xticks(x)
     ax.set_xticklabels(dates)
-    
+
     ax.axhline(y=totalAvgLines, linewidth=3, color='r')
     ax.legend()
-    
+
     def autolabel(bars):
         for bar in bars:
             height = bar.get_height()
             ax.annotate('{}'.format(height),
-                    xy=(bar.get_x() + bar.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
+                        xy=(bar.get_x() + bar.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
 
     autolabel(bar1)
     autolabel(bar2)
     fig.tight_layout()
-    
+
     dateGraph = datetime.now()
-    dateGraphWeekNumber = dateGraph.strftime('%W')
-    dateGraphWeek = int(dateGraphWeekNumber) - 1
+    dateGraphWeekNumber = dateGraph.strftime('%W') + 1
+    dateGraphWeek = int(dateGraphWeekNumber)
     plt.savefig(
         f'/home/dani/projects/rework_study/avg_week_{dateGraphWeek}_graph')
